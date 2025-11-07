f342780a0262becb1bba22b7b1e3d1c6ea332fac
Jowoever <jono.wink@gmail.com>
2025-11-07 18:23:24 +0100

    options test

--- nieuw-spelproject/info.gd
+++ nieuw-spelproject/info.gd
@@ -4,3 +4,7 @@ extends Button
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/info.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/jonoround.tscn")
