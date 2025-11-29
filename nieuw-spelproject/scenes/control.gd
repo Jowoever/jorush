5c5e94156b2b8710d8645060dcd97ac06db0e23e
Aarush <aarush.pothula@gmail.com>
2025-11-29 13:41:19 +0100

    audio sliders
    updated sliders

--- nieuw-spelproject/scenes/control.gd
+++ nieuw-spelproject/scenes/control.gd
@@ -1,4 +1,18 @@
-extends Control
+extends HSlider
 
-
-func _on_h_slider_value_changed(value: float) -> void: AudioServer.set_bus_volume_db(0, value)
+@export
+var bus_name: String
+var bus_index: int
+func _ready() -> void:
+		bus_index = AudioServer.get_bus_index(bus_name)
+		value_changed.connect(_on_value_changed)
+		
+		value = db_to_linear(
+			AudioServer.get_bus_volume_db(bus_index)
+		)
+		
+func _on_value_changed(value: float) -> void: 
+	AudioServer.set_bus_volume_db(
+			bus_index,
+			linear_to_db(value)
+		)
