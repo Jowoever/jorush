38a9efc0ffb8cfc1d300380073093e6a2d5c7d3b
Aarush <aarush.pothula@gmail.com>
2025-11-29 13:56:22 +0100

    button click sfx attempt

--- nieuw-spelproject/info.gd
+++ nieuw-spelproject/info.gd
@@ -3,15 +3,17 @@ extends Button
 
 
 func _on_pressed() -> void:
+	$Click.play()
 	get_tree().change_scene_to_file("res://scenes/info.tscn")
-
+	
 
 
 
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/jonoround.tscn")
-
+	$Click.play()
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
+	$Click.play()
