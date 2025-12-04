d2de2c37689308b0088a7fe850674b51abaf2b2e
Aarush <aarush.pothula@gmail.com>
2025-12-04 14:41:55 +0100

    right arrow input for car added
    play the level1 scene specifically to test
    
    remaining issues to fix/improve:
    -resolution and screen size
    -wheel suspension physics
    -background details

--- nieuw-spelproject/Code/player.gd
+++ nieuw-spelproject/Code/player.gd
@@ -1,1 +1,12 @@
 extends RigidBody2D
+
+var wheels = []
+var speed = 60000
+
+func _ready():
+	wheels = get_tree().get_nodes_in_group("wheel")
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		for wheel in wheels:
+			wheel.apply_torque_impulse(speed * delta * 60)
