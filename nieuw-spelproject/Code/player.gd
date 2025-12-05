ab1a1dbbf1143df8c3b86e96e399a1008f2f8135
Aarush <aarush.pothula@gmail.com>
2025-12-05 09:36:53 +0100

    functionality car complete
    zoom fixed

--- nieuw-spelproject/Code/player.gd
+++ nieuw-spelproject/Code/player.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var wheels = []
 var speed = 60000
+var max_speed = 50
 
 func _ready():
 	wheels = get_tree().get_nodes_in_group("wheel")
@@ -9,4 +10,9 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		for wheel in wheels:
-			wheel.apply_torque_impulse(speed * delta * 60)
+			if wheel.angular_velocity < max_speed:
+				wheel.apply_torque_impulse(speed * delta * 60)
+	if Input.is_action_pressed("ui_left"):
+		for wheel in wheels:
+			if wheel.angular_velocity > -max_speed:
+				wheel.apply_torque_impulse(-speed * delta * 60)
