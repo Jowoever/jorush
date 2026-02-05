84fe44578027d5e2967dfc5e9bdb6cd43181cc77
Jowoever <jono.wink@gmail.com>
2026-02-05 11:43:34 +0100

    physics getallen goed gezet

--- nieuw-spelproject/Code/player.gd
+++ nieuw-spelproject/Code/player.gd
@@ -2,18 +2,22 @@ extends RigidBody2D
 
 var wheels = []
 var speed = 60000
-var max_speed = 50
+var max_speed = 100
 
 func _ready():
 	wheels = get_tree().get_nodes_in_group("wheel")
+	
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		for wheel in wheels:
 			if wheel.angular_velocity < max_speed:
-				wheel.apply_torque_impulse(speed * delta * 67)
+				wheel.apply_torque_impulse(speed * delta * 25)
+		 
+			
 	if Input.is_action_pressed("ui_left"):
 		for wheel in wheels:
 			if wheel.angular_velocity > -max_speed:
-				wheel.apply_torque_impulse(-speed * delta * 67)
+				wheel.apply_torque_impulse(-speed * delta * 25)
+				
 	
