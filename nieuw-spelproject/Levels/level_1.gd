d89d83a48856fb96b4b8fa933ea69139e39c06f1
Jowoever <jono.wink@gmail.com>
2026-02-12 13:53:53 +0100

    coin scene created (before anime)

--- nieuw-spelproject/Levels/level_1.gd
+++ nieuw-spelproject/Levels/level_1.gd
@@ -1,6 +1,9 @@
-extends Area2D
+extends Node2D
 
-var value = 10
+var coins_collected = 0
+
+func add_coins(amount):
+	coins_collected += amount
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,10 +12,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_coin_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		get_tree().get_current_scene().add_coins(value)
-		
-	
