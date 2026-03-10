b65fe05b4718cde3d33aaa1abef9e8c599350a4f
Jowoever <jono.wink@gmail.com>
2026-03-10 15:36:23 +0100

    alles

--- nieuw-spelproject/Levels/level_1.gd
+++ nieuw-spelproject/Levels/level_1.gd
@@ -4,11 +4,22 @@ var coins_collected = 0
 
 func add_coins(amount):
 	coins_collected += amount
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$ui/TextureRect/Label.text = str(coins_collected)
+	
+func update_fuel_UI(value):
+	
+	var fuel_progress_bar : ProgressBar = $UI/Fuel/ProgressBar
+	var fuel_anim_player : AnimationPlayer = $UI/Fuel/AnimationPlayer
+	
+	#update progress bar with given value
+	fuel_progress_bar.value = value
+	
+	#change color hue as the fuel runs out
+	var stylebox = fuel_progress_bar.get_theme_stylebox("fill")
+	stylebox.bg_color.h = lerpf(0, 0.3, value / 100)
+	
+	#play alarm animation
+	if value == 0:
+		fuel_anim_player.play("alarm")
+	else:
+		fuel_anim_player.play("RESET")
